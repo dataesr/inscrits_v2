8117addf1ab830d3a4cb8f7176a41b783676b9df
zoe <zmenesr@gmail.com>
2025-07-15 14:44:42 +0200

    .

--- library_import.py
+++ library_import.py
@@ -1,6 +1,7 @@
 from step1_data_init.sise_read import *
 from step1_data_init.sise_content import *
 from utils.bcn_load import *
+from utils.googleSheet import *
 from step2_first_control.variables_check import *
 from step2_first_control.etabli import *
 from utils.functions_shared import *
--- main.py
+++ main.py
@@ -6,23 +6,26 @@ INITIALISATION = False
 if INITIALISATION == True:
     # read sise file, select vars, create of excel files to check data, save concatenate data into parquet file per year
     sise_read(PATH)
+    get_all_correctifs_from_google()
 
 
 last_data_year = last_year_into_zip(f"{PATH}output/")
+
+global PAYSAGE_id, CORRECTIFS_dict, BCN
 
 # STEP 2 - etabli + paysage
 if 'paysage_id' in globals():
     print("paysage_id exists.")
 else:
     print("paysage_id does not exist. Loading it now.")
-    global paysage_id
-    paysage_id = get_paysageODS("fr-esr-paysage_structures_identifiants")
+    PAYSAGE_id = get_paysageODS("fr-esr-paysage_structures_identifiants")
     
-etabli = etabli_paysage(last_data_year, paysage_id)
+etabli = etabli_paysage(last_data_year, PAYSAGE_id)
 
-# load BCN
-bcn = bcn_complete()
-modal_sise = vars_sise_to_be_check(last_data_year, bcn)
+
+CORRECTIFS_dict = get_all_correctifs()
+BCN = bcn_complete()
+modal_sise = vars_sise_to_be_check(last_data_year, BCN, CORRECTIFS_dict)
 
 # from test_py import work_csv
 # work_csv(x, 'vars_hs_nomen')
