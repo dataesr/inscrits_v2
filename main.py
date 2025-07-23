182260dfb3ad6bed5d1db8a3d7fffbaf63d5bab7
zoe <zmenesr@gmail.com>
2025-07-23 07:21:07 +0200

    switch code line

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ INITIALISATION = False
 if INITIALISATION == True:
     # read sise file, select vars, create of excel files to check data, save concatenate data into parquet file per year
     # sise_config()
-    sise_read(PATH)
     get_all_correctifs_from_google()
     bcn_complete()
     get_paysageODS("fr-esr-paysage_structures_identifiants")
+    sise_read(PATH)
 
+
+# STEP 2
 last_data_year = last_year_into_zip(f"{PATH}output/")
 
 vars_sise_to_be_check(last_data_year)
