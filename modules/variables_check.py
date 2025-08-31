98b88c9624b6769acaca0d7b5bade1917dceea3a
zoe <zmenesr@gmail.com>
2025-08-31 10:38:31 +0200

    move etabs process to etablissement script

--- modules/variables_check.py
+++ modules/variables_check.py
@@ -1,47 +1,22 @@
-from nomenclatures.paysage import *
 from nomenclatures.bcn import *
 from nomenclatures.google_sheet import *
-from utils.functions_shared import nomenclatures_load
+from nomenclatures.nomenclatures import nomenclatures_load
 from config_path import PATH
 import pandas as pd, json, numpy as np
 
 global CORRECTIFS_dict, BCN, PAYSAGE_id
 CORRECTIFS_dict = nomenclatures_load('google_sheet')
 BCN = nomenclatures_load('bcn')
-PAYSAGE_id = nomenclatures_load('paysage_id')
-
-
-def uai_paysage(df, var):
-    x=(df.merge(pd.DataFrame(PAYSAGE_id)[['id_value', 'id_paysage', 'active','id_enddate']], 
-                how='left', left_on=var, right_on='id_value')
-        .drop_duplicates()
-        .merge(BCN['N_BCE_SISE_N'][['numero_uai', 'denomination_principale_uai']].drop_duplicates(), 
-               how='left', left_on=var, right_on='numero_uai'))
-
-    x=x.assign(paysage_presence=np.where(x.id_value.isnull(), 'N', 'Y'), variable=var)
-    x.loc[~x.id_enddate.isnull(), 'end_year'] = x.loc[~x.id_enddate.isnull()].id_enddate.str.split('-').str[0]
-    x['pid_multi']=x.groupby(['rentree', 'source', var]).transform('size')
-    return x.drop(columns=['numero_uai', 'id_value'])
 
 
 def vars_sise_to_be_check(year):
-    from modules.variables_check import uai_paysage
 
     CONF = json.load(open('utils/config_sise.json', 'r'))
-    uai = pd.read_pickle(f"{PATH}output/frequency_uai_source_year{year}.pkl",compression= 'gzip')
     vars_sise = pd.read_pickle(f"{PATH}output/items_by_vars{year}.pkl", compression='gzip')
-
-    uai_pays = pd.DataFrame()
-    up = uai_paysage(uai, 'compos')
-    uai_pays = pd.concat([uai_pays, up], ignore_index=True)
-    up = uai_paysage(uai, 'etabli')
-    up = up.loc[~up.etabli.isin(uai_pays.compos.unique())]
-    uai_pays = pd.concat([uai_pays, up], ignore_index=True)
-
     hors_nomen=pd.DataFrame()
 
     for conf in CONF:
-        if conf['var_sise'] not in ['etabli', 'compos']:
+        if conf['var_sise'] not in ['etabli', 'compos', 'etabli_diffusion']:
             var_sise = conf["var_sise"]
             nomen = conf["n_data"]
 
@@ -76,4 +51,3 @@ def vars_sise_to_be_check(year):
 			 
     with pd.ExcelWriter(f"{PATH}vars_review_{year}.xlsx", mode='w', engine="openpyxl") as writer:  
         hors_nomen.to_excel(writer, sheet_name='vars_hs_norme', index=False,  header=True, na_rep='', float_format='str')
-        uai_pays.to_excel(writer, sheet_name='uai_paysage', index=False,  header=True, na_rep='')
