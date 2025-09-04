88a5913f956c9723f2c706943893d5dcda306ce5
zoe <zmenesr@gmail.com>
2025-09-04 12:24:36 +0200

    etab cleaning process continue

--- modules/etab_cleaner.py
+++ modules/etab_cleaner.py
@@ -1,12 +1,18 @@
 from reference_data.ref_data_utils import reference_data_loader
 from config_path import PATH
-import pandas as pd, json, numpy as np, pyreadstat
+import pandas as pd, json, numpy as np
 
 global CORRECTIFS_dict, BCN, PAYSAGE_id
 CORRECTIFS_dict = reference_data_loader('google_sheet')
 BCN = reference_data_loader('bcn')
 PAYSAGE_id = reference_data_loader('paysage_id')
 
+def uai_bcn():
+    b=BCN['N_BCE_SISE_N']
+    for i in ['etabli', 'compos', 'rattach']:
+        b[i]=b["numero_uai"]
+        b[f"lib_{i}"]=b["appellation_officielle_uai"]
+    return b
 
 def uai_fixing(df):
     uai_fix=json.load(open('patches/uai_fixes.json', 'r'))
@@ -23,6 +29,52 @@ def uai_fixing(df):
             df.loc[mask, vout] = fix[vout]
             df.loc[mask, 'rattach'] = np.nan
     return df
+
+
+def uai_patching(etab, patch_dict):
+    if patch_dict == 'compos_empty':
+        uai_v = ['etabli', 'compos']
+    if patch_dict == 'rattach_patch':
+        uai_v = ['compos', 'rattach']
+
+    map_dict = json.load(open(f"patches/{patch_dict}.json", 'r'))
+    mapping = (
+            pd.DataFrame([
+            {"rentree": int(rentree), uai_v[0]: vin, uai_v[1]:vout}
+            for rentree, dico in map_dict.items()
+            for vin, vout in dico.items()
+            ])
+        )
+    
+    etab = etab.merge(mapping, how='left', on=["rentree", uai_v[0]], suffixes=('', '_y'))
+
+    if patch_dict == 'compos_empty':
+        etab.loc[(etab[uai_v[1]].isna()) | (etab[uai_v[1]] == ''), uai_v[1]] = etab.loc[(etab[uai_v[1]].isna()) | (etab[uai_v[1]] == ''), f'{uai_v[1]}_y']
+    else:
+        etab.loc[~etab[f'{uai_v[1]}_y'].isnull(), uai_v[1]] = etab.loc[~etab[f'{uai_v[1]}_y'].isnull(), f'{uai_v[1]}_y']
+
+
+    # uai_empty_patch_checking
+    etab_null = etab.loc[(etab[uai_v[1]].isna()) | (etab[uai_v[1]] == '')]
+    if len(etab_null)>0:
+        print(f"- reste {uai_v[1]} ; compléter le dict patches/{patch_dict}.json:\n{etab_null[['rentree', uai_v[0], uai_v[1]]]}")
+        tmp=(
+                etab_null.groupby("rentree")
+                .apply(lambda x: dict(zip(x[uai_v[0]], x[uai_v[1]])))
+                .to_dict()
+            )
+        for annee, valeurs in tmp.items():
+            if str(annee) in map_dict:
+                map_dict[str(annee)].update(valeurs)
+            else:
+                map_dict[str(annee)] = valeurs
+
+        # Écrire le résultat dans le fichier
+        with open(f'patches/{patch_dict}.json', 'w') as f:
+            json.dump(map_dict, f, indent=4)
+    
+    return etab.drop(columns=f'{uai_v[1]}_y')
+
 
 def code_uai_safe_checking(code_uai):
     alphabet_23 = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -49,82 +101,63 @@ class CustomInterrupt(Exception):
     pass
 
 
-def rattach_fixing(year):
-    rattach_df = pd.read_pickle(f"{PATH}output/uai_frequency_source_year{year}.pkl",compression= 'gzip')
-    rattach_df = rattach_df[['rentree', 'source', 'etabli', 'compos', 'rattach']]
+def uai_invalid_fix(etab):
+    for i in ['etabli', 'compos', 'rattach']:
+        etab[f'{i}_valide'] = etab[i].apply(code_uai_safe_checking)
 
-    # COMPOS empty
-    if len(rattach_df.loc[rattach_df.compos.isnull()])>0:
-        empty = json.load(open('patches/compos_empty.json', 'r'))
-        mapping = (
-                pd.DataFrame([
-                {"rentree": int(rentree), "etabli": etabli, 'compos':compos}
-                for rentree, dico in empty.items()
-                for etabli, compos in dico.items()
-                ])
-            )
-        
-        rattach_df = rattach_df.merge(mapping, how='left', on=["rentree", "etabli"], suffixes=('', '_y'))
-        rattach_df.loc[(rattach_df['compos'].isna()) | (rattach_df['compos'] == ''), 'compos'] = rattach_df.loc[(rattach_df['compos'].isna()) | (rattach_df['compos'] == ''), 'compos_y']
-        rattach_df.drop(columns='compos_y', inplace=True)
-        
-        rattach_null = rattach_df.loc[(rattach_df['compos'].isna()) | (rattach_df['compos'] == '')]
-        if len(rattach_null)>0:
-            print(f"- après maj RATTACH il en manque encore ; compléter le dict patches/rattach_patch.json:\n{rattach_null}")
-            tmp=(
-                    rattach_null.groupby("rentree")
-                    .apply(lambda x: dict(zip(x["etabli"], x["compos"])))
-                    .to_dict()
-                )
-            for annee, valeurs in tmp.items():
-                if str(annee) in empty:
-                    empty[str(annee)].update(valeurs)
-                else:
-                    empty[str(annee)] = valeurs
+    df_invalides = etab[~etab['etabli_valide'] | ~etab['compos_valide'] | ~etab['rattach_valide']]
+    df_invalides[['rentree','source','etabli', 'etabli_valide', 'compos', 'compos_valide', 'rattach', 'rattach_valide','effectif_tot']]
+    
+    # Extraire les codes UAI distincts invalides des colonnes 'etabli' et 'compos'
+    invalid_etabli_codes = etab.loc[~etab['etabli_valide'], 'etabli'].unique()
+    invalid_compos_codes = etab.loc[~etab['compos_valide'], 'compos'].unique()
+    invalid_rattach_codes = etab.loc[~etab['rattach_valide'], 'rattach'].unique()
 
-            # Écrire le résultat dans le fichier
-            with open('compos_empty.json', 'w') as f:
-                json.dump(empty, f, indent=4)
+    # Combiner et obtenir les codes distincts
+    invalid_uai_codes = list(set(invalid_etabli_codes).union(set(invalid_compos_codes)).union(set(invalid_rattach_codes)))
+    print(f"- size UAI_INVALID:{len(invalid_uai_codes)}\n{invalid_uai_codes}")
 
-    # UAI wrong
-    rattach_df = uai_fixing(rattach_df)
+    liste_filtree = [mot for mot in invalid_uai_codes if len(mot) < 4 or mot[3] != 'P']
+    if len(liste_filtree)>0:
+        masque = pd.DataFrame(False, index=etab.index, columns=etab.columns)
+        for s in liste_filtree:
+            masque = masque | etab.isin([s])
+
+        # Filtrer le DataFrame original avec le masque
+        resultat_df = etab[masque.any(axis=1)]
+        print(f"- uai to fix in patches:\n{resultat_df[['rentree', 'etabli', 'etabli_valide', 'compos', 'compos_valide', 'rattach', 'rattach_valide']]}")
+
+    uai_ref = uai_bcn() 
+    for i in ['etabli', 'compos', 'rattach']:
+        etab = pd.merge(etab, uai_ref[[i, f"lib_{i}"]], how='left', on=[i])
+    
+    return etab
+
+
+def etab_update(year):
+
+    etab = pd.read_pickle(f"{PATH}output/uai_frequency_source_year{year}.pkl",compression= 'gzip')
+    # etab = etab_df[['rentree', 'source', 'etabli', 'compos', 'rattach', 'effectif_tot']]
+    print(f"- first size ETAB: {len(etab)}")
+
+    # UAI wrong -> ETABLI=ETABLI_OLD & COMPOS=COMPOS_OLD
+    etab = uai_fixing(etab)
     for i in ['etabli', 'compos']:
-        rattach_df.loc[rattach_df[f"{i}_new"].isnull(), f"{i}_new"] = rattach_df.loc[rattach_df[f"{i}_new"].isnull(), i]
+        etab.loc[etab[f"{i}_new"].isnull(), f"{i}_new"] = etab.loc[etab[f"{i}_new"].isnull(), i]
+        etab = etab.rename(columns={i: f"{i}_old", f"{i}_new": i})
 
-    # rattach_df['etabli_valide'] = rattach_df['etabli_new'].apply(code_uai_safe_checking)
-    # rattach_df['compos_valide'] = rattach_df['compos_new'].apply(code_uai_safe_checking)
+    # COMPOS_NEW empty
+    etab = uai_patching(etab, 'compos_empty')
+    print(f"- size ETAB after cleaning COMPOS: {len(etab)}")
 
-    # RATTACH empty
-    rattach_patch=json.load(open('patches/rattach_patch.json', 'r'))
-    mapping = (
-        pd.DataFrame([
-            {"rentree": int(rentree), "compos_new": compos, 'rattach':rattach}
-            for rentree, dico in rattach_patch.items()
-            for compos, rattach in dico.items()
-        ])
-    )
-    # corrections rattachements vides
-    rattach_df = rattach_df.merge(mapping, how='left', on=["rentree", "compos_new"], suffixes=('', '_y'))
-    rattach_df.loc[(rattach_df['rattach'].isna()) | (rattach_df['rattach'] == ''), 'rattach'] = rattach_df.loc[(rattach_df['rattach'].isna()) | (rattach_df['rattach'] == ''), 'rattach_y']
-    rattach_df.drop(columns='rattach_y', inplace=True)
+    # RATTACH empty or wrong
+    etab = uai_patching(etab, 'rattach_patch')
+    print(f"- size ETAB after cleaning RATTACH: {len(etab)}")
 
-    rattach_null = rattach_df[rattach_df['rattach'].isna() | (rattach_df['rattach'] == '')]
-    if len(rattach_null)>0:
-        print(f"- après maj RATTACH il en manque encore ; compléter le dict patches/rattach_patch.json:\n{rattach_null}")
-        tmp=(
-                rattach_null.groupby("rentree")
-                .apply(lambda x: dict(zip(x["compos_new"], x["rattach"])))
-                .to_dict()
-            )
-        for annee, valeurs in tmp.items():
-            if str(annee) in rattach_patch:
-                rattach_patch[str(annee)].update(valeurs)
-            else:
-                rattach_patch[str(annee)] = valeurs
+    # check uai validity
+    etab = uai_invalid_fix(etab)
 
-        # Écrire le résultat dans le fichier
-        with open('patches/rattach_patch.json', 'w') as f:
-            json.dump(rattach_patch, f, indent=4)
+    return etab
 
 
 def etabli_meef(year):
