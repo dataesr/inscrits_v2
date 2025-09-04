2ec0ab22ad06931f1d1d1a45efe021c08ef2145b
zoe <zmenesr@gmail.com>
2025-09-04 18:15:20 +0200

    add loop for all rentree

--- main.py
+++ main.py
@@ -15,12 +15,6 @@ if INITIALISATION == True:
     # get_paysageODS("fr-esr-paysage_structures_identifiants", "paysage_id")
     
 
-
-# # chargement des nomenclatures
-# CORRECTIFS_dict = reference_data_loader('google_sheet')
-# BCN = reference_data_loader('bcn')
-# PAYSAGE_id = reference_data_loader('paysage_id') 
-
 # STEP 2
 last_data_year = last_year_into_zip(f"{PATH}output/")
 
@@ -28,10 +22,13 @@ etab = etab_update(last_data_year)
 meef = etabli_meef(last_data_year)
 
 
-vars_sise_to_be_check(last_data_year)
+# vars_sise_to_be_check(last_data_year)
 
+ALL_RENTREES = list(range(2004, int(last_data_year)+1))
+for rentree in ALL_RENTREES:
+    df = data_cleansing(last_data_year, rentree, etab, meef)
 
-df=data_cleansing(2024, 2024, meef)
+    
 # si besoin de vérifier une source spécifique sans traitement
 # df=get_individual_source('inge', '2016')
 # from test_py import work_csv
