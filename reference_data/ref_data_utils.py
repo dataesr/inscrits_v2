cd83609e52b500a950eb423a2f296efc04992948
zoe <zmenesr@gmail.com>
2025-09-11 09:42:04 +0200

    script to call dictionary into all project

--- reference_data/ref_data_utils.py
+++ reference_data/ref_data_utils.py
@@ -1,14 +1,17 @@
-def reference_data_loader(nomen):
-    if nomen.lower()=='bcn':
-        from reference_data.bcn import get_all_bcn
-        return get_all_bcn()
-    if nomen.lower().startswith('paysage'):
-        from reference_data.paysage import get_paysage
-        return get_paysage(nomen)
-    if nomen.lower()=='google_sheet':
-        from reference_data.google_sheet import get_all_correctifs
-        return get_all_correctifs()
-    
+# def reference_data_loader(nomen):
+#     if nomen.lower()=='bcn':
+#         from reference_data.bcn import get_all_bcn
+#         return get_all_bcn()
+#     if nomen.lower().startswith('paysage'):
+#         from reference_data.paysage import get_paysage
+#         return get_paysage(nomen)
+#     if nomen.lower()=='google_sheet':
+#         from reference_data.google_sheet import get_all_correctifs
+#         return get_all_correctifs()
+from reference_data.bcn import get_all_bcn
+from reference_data.google_sheet import get_all_correctifs
+from reference_data.paysage import get_paysage
 
-def lookup_table(ref, table:str):
-    return ref[table]
+CORRECTIFS_dict = get_all_correctifs()
+PAYSAGE_dict = get_paysage('paysage')
+BCN = get_all_bcn()
