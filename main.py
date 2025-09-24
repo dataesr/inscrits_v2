f6e777142dcd3fc54bef0a4322514b83ac3f81c1
zoe <zmenesr@gmail.com>
2025-09-24 10:28:36 +0200

    .

--- main.py
+++ main.py
@@ -26,40 +26,26 @@ etab = etab_update(last_data_year)
 etab = enrich_paysage(etab)
 meef = etabli_meef(last_data_year)
 
-
 # vars_sise_to_be_check(last_data_year)
+
 
 zipin_path = os.path.join(PATH, f"output/sise_parquet_{last_data_year}.zip")
 zipout_path = os.path.join(PATH, f"output/sise_cleaned_{last_data_year}.zip")
 
 df_all = pd.DataFrame()
-df_saclay = pd.DataFrame()
 
 ALL_RENTREES = list(range(2004, int(last_data_year)+1))
 for rentree in ALL_RENTREES:
-# for rentree in [2024]:
     df = get_individual_source(zipin_path, 'sise', rentree)
     df = data_cleansing(df, etab, meef)
-    df_all = pd.concat([df_all, df[['rentree', 'source', 'etabli', 'compos', 'rattach', 'id_paysage', 'lib_paysage', 'inspr', 'effectif']]])
-    df_saclay = pd.concat([df_saclay, df.loc[df.etabli=='0912408Y', ['rentree', 'source', 'compos', 'typ_dipl', 'diplom', 'inspr', 'effectif']]])
+    df_all = pd.concat([df_all, df[['rentree', 'source', 'etabli', 'compos', 'rattach', 'id_paysage', 'lib_paysage', 'inspr', 'typ_dipl', 'diplom', 'effectif']]])
     data_save_by_year(rentree, df, 'sise', zipout_path)
 
-df_saclay.to_pickle(f"{PATH}output/saclay_data_{last_data_year}.pkl", compression='gzip')
-
-x = (df_all.loc[df_all.inspr=='O']
-            .groupby(list(set(df_all.columns).difference(set(['effectif']))), dropna=False)
-            .agg({'effectif': 'sum'})
-            .reset_index()
-            .reindex(columns=['rentree', 'source', 'etabli', 'id_paysage', 'lib_paysage', 'rattach', 'compos', 'effectif'])
-
-            )
-x.to_pickle(f"{PATH}output/effectif_by_etab_rentree.pkl", compression='gzip')
+etab_checking(int(last_data_year), df_all)
 
 # si besoin de vérifier une source spécifique sans traitement
-zip_path=os.path.join(PATH, f"output/sise_parquet_{last_data_year}.zip")
-df=get_individual_source(zip_path,'sise', '2016')
+# zip_path=os.path.join(PATH, f"output/sise_parquet_{last_data_year}.zip")
+zip_path=os.path.join(PATH, f"input/parquet_origine.zip")
+df=get_individual_source(zip_path,'culture', '2024')
 # from test_py import work_csv
 # work_csv(x, 'vars_hs_nomen')
-
-
-
