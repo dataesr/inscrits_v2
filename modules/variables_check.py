322afa3e7fac99750e0438336cc414aec2bb269b
zoe <zmenesr@gmail.com>
2025-10-16 09:01:06 +0200

    check cursus, diplom, com_code

--- modules/variables_check.py
+++ modules/variables_check.py
@@ -7,23 +7,41 @@ import pandas as pd, json, numpy as np
 
 
 
-def diplom_to_check_by_source(year, hors_nomen):
-    zip_path=os.path.join(PATH, f"output/sise_cleaned_{year}.zip")
+def cursus_to_check(year, df):
+    t1 = (df.loc[df.cursus_lmd.isin(['-1', 'X', '9'])]
+            .drop(columns='effectif')
+            .drop_duplicates())
+    with pd.ExcelWriter(f"{PATH}vars_review_cleaned_{year}.xlsx", mode='a', if_sheet_exists="replace") as writer:  
+        t1.to_excel(writer, sheet_name='cursus_lmd', index=False)
 
-    # diplom
-    df = pd.DataFrame()
-    dip = hors_nomen.loc[(hors_nomen.variable=='diplom')&(hors_nomen.hors_nomenclature=='1'), ['rentree', 'source', 'item']]
 
-    for rentree in dip.rentree.unique():
-        tmp = get_individual_source(zip_path, 'sise', rentree)
-        tmp = tmp.merge(dip.loc[(dip.rentree==rentree)], how='inner', left_on=['rentree', 'source', 'diplom'], right_on=['rentree', 'source', 'item'])
-        df = pd.concat([df, tmp[['rentree', 'source', 'etabli', 'id_paysage', 'lib_paysage', 'compos', 'typ_dipl', 'diplom', 'sectdis', 'cursus_lmd']].drop_duplicates()], ignore_index=True)
+def vars_compare_with_bcn(year, df):
+    t1 = (df.loc[(df.typ_dipl!=df.typ_dipl_orig)|(df.sectdis!=df.sectdis_orig)]
+          .drop(columns='effectif')
+          .drop_duplicates())
+    t1 = t1.loc[~(t1.typ_dipl.isnull()&t1.sectdis.isnull())]
 
     with pd.ExcelWriter(f"{PATH}vars_review_cleaned_{year}.xlsx", mode='a', if_sheet_exists="replace") as writer:  
-        df.to_excel(writer, sheet_name='diplom', index=False)
+        t1.to_excel(writer, sheet_name='vars_difference', index=False)
 
 
-def vars_sise_to_be_check(year, stage):
+def diplom_to_check_by_source(year, hors_nomen, df):
+    # zip_path=os.path.join(PATH, f"output/sise_cleaned_{year}.zip")
+
+    # diplom
+    tmp = pd.DataFrame()
+    dip = hors_nomen.loc[(hors_nomen.variable=='diplom')&(hors_nomen.hors_nomenclature=='1'), ['rentree', 'source', 'item']]
+
+    # for rentree in dip.rentree.unique():
+    # tmp = get_individual_source(zip_path, 'sise', rentree)
+    df = df.merge(dip, how='inner', left_on=['rentree', 'source', 'diplom'], right_on=['rentree', 'source', 'item'])
+    tmp = pd.concat([tmp, df[['rentree', 'source', 'etabli', 'id_paysage', 'lib_paysage', 'compos', 'typ_dipl', 'typ_dipl_orig', 'diplom', 'sectdis', 'sectdis_orig', 'cursus_lmd']].drop_duplicates()], ignore_index=True)
+
+    with pd.ExcelWriter(f"{PATH}vars_review_cleaned_{year}.xlsx", mode='a', if_sheet_exists="replace") as writer:  
+        tmp.to_excel(writer, sheet_name='diplom', index=False)
+
+
+def vars_sise_to_be_check(year, stage, df):
 
     CONF = json.load(open('utils/config_sise.json', 'r'))
     vars_sise = pd.read_pickle(f"{PATH}output/items_{stage}_by_vars{year}.pkl", compression='gzip')
@@ -55,9 +73,7 @@ def vars_sise_to_be_check(year, stage):
                     l=BCN[nomen][[BCN[nomen].columns[0], libelle_columns[0]]].rename(columns={BCN[nomen].columns[0]:'item', libelle_columns[0]:'libelle'})
         
         if nomen in CORRECTIFS_dict:
-            if var_sise in ['cometa', 'comins']:
-                l = pd.DataFrame.from_dict(CORRECTIFS_dict['LES_COMMUNES'])[['com_code', 'com_nom']]
-            else:
+            if var_sise not in ['cometa', 'comins']:
                 l = pd.DataFrame.from_dict(CORRECTIFS_dict[nomen]).iloc[:, :2]
             l.columns = ['item', 'libelle']
             
@@ -73,10 +89,42 @@ def vars_sise_to_be_check(year, stage):
         hors_nomen.to_excel(writer, sheet_name='vars_hs_norme', index=False,  header=True, na_rep='', float_format='str')
 
     if stage=='cleaned':
-        diplom_to_check_by_source(year, hors_nomen)
+        diplom_to_check_by_source(year, hors_nomen, df)
+        vars_compare_with_bcn(year, df)
+        cursus_to_check(year, df)
 
 
 
+def commune_checking(year, df):
+    l = pd.DataFrame.from_dict(CORRECTIFS_dict['LES_COMMUNES'])[['com_code', 'com_nom']]
+    for i in ['cometa', 'comins']:
+
+        if i=='cometa':
+            cols = list(set(df.columns).difference(set(['compos', 'comins'])))
+            tmp = df[cols].drop_duplicates()
+        else:
+            tmp = df.copy()
+
+        tmp = (tmp.merge(l, how='left', left_on=i, right_on='com_code')
+                .rename(columns={'com_nom':f'{i}_name'})
+                .drop(columns='com_code')
+            )
+
+
+        if i == 'comins':
+            tmp = tmp.assign(hors_nomenclature=np.where(tmp[f"{i}_name"].isnull(), '1', '0')).sort_values(['rentree', 'id_paysage', 'compos'])
+        else:
+            tmp = tmp.assign(hors_nomenclature=np.where(tmp[f"{i}_name"].isnull(), '1', '0')).sort_values(['rentree', 'id_paysage'])
+
+        tmp['nb']=tmp.groupby(list(set(tmp.columns).difference(set([i]))), dropna=False).transform('size')
+
+        if os.path.isfile(os.path.join(PATH, 'vars_communes.xlsx')):
+            with pd.ExcelWriter(f"{PATH}vars_communes.xlsx", mode='a', engine="openpyxl", if_sheet_exists="replace") as writer:  
+                tmp.to_excel(writer, sheet_name=i, index=False,  header=True, na_rep='')
+        else:
+            with pd.ExcelWriter(f"{PATH}vars_communes.xlsx", mode='w', engine="openpyxl") as writer:  
+                tmp.to_excel(writer, sheet_name=i, index=False,  header=True, na_rep='')
+    
 
 def etab_checking(year, df):
     
@@ -99,14 +147,14 @@ def etab_checking(year, df):
         'YI', 'DR']
 
     y1 = df.loc[(df.inspr=='O')&(df.typ_dipl.isin(dd)), 
-                ['rentree', 'source', 'etabli', 'id_paysage', 'lib_paysage', 'rattach', 'compos', 'typ_dipl', 'diplom', 'effectif']]
+                ['rentree', 'source', 'etabli', 'id_paysage', 'lib_paysage', 'rattach', 'compos', 'typ_dipl', 'typ_dipl_orig', 'diplom', 'effectif']]
     y1 = (y1.groupby(list(set(y1.columns).difference(set(['effectif']))), dropna=False)
             .agg({'effectif': 'sum'})
             .reset_index()
             )
 
     y2 = df.loc[(df.inspr=='O')&(~df.typ_dipl.isin(dd)), 
-                ['rentree', 'source', 'etabli', 'id_paysage', 'lib_paysage', 'rattach', 'compos', 'typ_dipl', 'effectif']]
+                ['rentree', 'source', 'etabli', 'id_paysage', 'lib_paysage', 'rattach', 'compos', 'typ_dipl', 'typ_dipl_orig', 'effectif']]
     y2 = (y2.groupby(list(set(y2.columns).difference(set(['effectif']))), dropna=False)
             .agg({'effectif': 'sum'})
             .reset_index()
