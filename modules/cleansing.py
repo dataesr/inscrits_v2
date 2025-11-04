3b654c11ebd65fa948b1a92357bdaf9141f7e9c1
zoe <zmenesr@gmail.com>
2025-11-04 16:33:35 +0100

    move etab cleansing from cleansing to etab script

--- modules/cleansing.py
+++ modules/cleansing.py
@@ -1,9 +1,224 @@
 from config_path import PATH
-from reference_data.ref_data_utils import CORRECTIFS_dict, BCN, PAYSAGE_dict
+from reference_data.ref_data_utils import CORRECTIFS_dict, BCN
 from utils.functions_shared import replace_by_nan
 import json, pandas as pd, numpy as np
 
 
+############################################################################
+# ETABLISSEMENTS
+############################################################################
+def uai_bcn():
+    b=BCN['N_BCE_SISE_N']
+    for i in ['etabli', 'compos', 'rattach']:
+        b[i]=b["numero_uai"]
+        b[f"lib_{i}"]=b["appellation_officielle_uai"]
+    return b
+
+def uai_fixing(df):
+    uai_fix=json.load(open('patches/uai_fixes.json', 'r'))
+    df['compos_new'] = None
+    df['etabli_new'] = None
+
+    # Appliquer les corrections
+    for annee, fixes in uai_fix.items():
+        annee_int = int(annee)
+        for fix in fixes:
+            vin = list(fix.keys())[0]
+            vout = list(fix.keys())[1]
+            mask = (df['rentree'] == annee_int) & (df[vin] == fix[vin])
+            df.loc[mask, vout] = fix[vout]
+            df.loc[mask, 'rattach'] = np.nan
+    return df
+
+def uai_patching(etab, patch_dict):
+    if patch_dict == 'compos_empty':
+        uai_v = ['etabli', 'compos']
+    if patch_dict == 'rattach_patch':
+        uai_v = ['compos', 'rattach']
+    if patch_dict == 'etabli_patch':
+        uai_v = ['rattach', 'etabli']
+
+    map_dict = json.load(open(f"patches/{patch_dict}.json", 'r'))
+    mapping = (
+            pd.DataFrame([
+            {"rentree": int(rentree), uai_v[0]: vin, uai_v[1]:vout}
+            for rentree, dico in map_dict.items()
+            for vin, vout in dico.items()
+            ])
+        )
+    
+    etab = etab.merge(mapping, how='left', on=["rentree", uai_v[0]], suffixes=('', '_y'))
+
+    if patch_dict == 'compos_empty':
+        etab.loc[(etab[uai_v[1]].isna()) | (etab[uai_v[1]] == ''), uai_v[1]] = etab.loc[(etab[uai_v[1]].isna()) | (etab[uai_v[1]] == ''), f'{uai_v[1]}_y']
+    else:
+        etab.loc[~etab[f'{uai_v[1]}_y'].isnull(), uai_v[1]] = etab.loc[~etab[f'{uai_v[1]}_y'].isnull(), f'{uai_v[1]}_y']
+
+
+    # uai_empty_patch_checking
+    etab_null = etab.loc[(etab[uai_v[1]].isna()) | (etab[uai_v[1]] == '')]
+    if len(etab_null)>0:
+        print(f"- reste {uai_v[1]} ; compléter le dict patches/{patch_dict}.json:\n{etab_null[['rentree', uai_v[0], uai_v[1]]]}")
+        tmp=(
+                etab_null.groupby("rentree")
+                .apply(lambda x: dict(zip(x[uai_v[0]], x[uai_v[1]])))
+                .to_dict()
+            )
+        for annee, valeurs in tmp.items():
+            if str(annee) in map_dict:
+                map_dict[str(annee)].update(valeurs)
+            else:
+                map_dict[str(annee)] = valeurs
+
+        # Écrire le résultat dans le fichier
+        with open(f'patches/{patch_dict}.json', 'w') as f:
+            json.dump(map_dict, f, indent=4)
+    
+    return etab.drop(columns=f'{uai_v[1]}_y')
+
+def code_uai_safe_checking(code_uai):
+    alphabet_23 = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    if isinstance(code_uai, str):
+        code_uai_propre = code_uai.strip().lower()
+    else:
+        print(code_uai)
+        
+    # Vérifier si les 7 premiers caractères sont numériques
+    if len(code_uai_propre) < 8 or not code_uai_propre[:7].isdigit():
+        return False
+    
+    code_uai_propre_chiffres = code_uai_propre[:7]
+    code_uai_propre_lettre = code_uai_propre[7:8]
+    
+    try:
+        rang_calcule = int(code_uai_propre_chiffres) % 23
+        lettre_calculee = alphabet_23[rang_calcule]
+        return code_uai_propre_lettre == lettre_calculee
+    except ValueError:
+        return False
+class CustomInterrupt(Exception):
+    pass
+
+def uai_invalid_fix(etab):
+    for i in ['etabli', 'compos', 'rattach']:
+        etab[f'{i}_valide'] = etab[i].apply(code_uai_safe_checking)
+
+    df_invalides = etab[~etab['etabli_valide'] | ~etab['compos_valide'] | ~etab['rattach_valide']]
+    df_invalides[['rentree','source','etabli', 'etabli_valide', 'compos', 'compos_valide', 'rattach', 'rattach_valide','effectif_tot']]
+    
+    # Extraire les codes UAI distincts invalides des colonnes 'etabli' et 'compos'
+    invalid_etabli_codes = etab.loc[~etab['etabli_valide'], 'etabli'].unique()
+    invalid_compos_codes = etab.loc[~etab['compos_valide'], 'compos'].unique()
+    invalid_rattach_codes = etab.loc[~etab['rattach_valide'], 'rattach'].unique()
+
+    # Combiner et obtenir les codes distincts
+    invalid_uai_codes = list(set(invalid_etabli_codes).union(set(invalid_compos_codes)).union(set(invalid_rattach_codes)))
+    print(f"- size UAI_INVALID:{len(invalid_uai_codes)}\n{invalid_uai_codes}")
+
+    liste_filtree = [mot for mot in invalid_uai_codes if len(mot) < 4 or mot[3] != 'P']
+    if len(liste_filtree)>0:
+        masque = pd.DataFrame(False, index=etab.index, columns=etab.columns)
+        for s in liste_filtree:
+            masque = masque | etab.isin([s])
+
+        # Filtrer le DataFrame original avec le masque
+        resultat_df = etab[masque.any(axis=1)]
+        print(f"- uai to fix in patches:\n{resultat_df[['rentree', 'etabli', 'etabli_valide', 'compos', 'compos_valide', 'rattach', 'rattach_valide']]}")
+
+    uai_ref = uai_bcn() 
+    for i in ['etabli', 'compos', 'rattach']:
+        etab = pd.merge(etab, uai_ref[[i, f"lib_{i}"]], how='left', on=[i])
+    
+    return etab
+
+#####################################################
+## COMMUNES ####################################
+def cometa_clean(df):
+    try:
+        print(f"- COMETA clean")
+        x=pd.DataFrame(CORRECTIFS_dict['A_COMETA']) 
+        x['rentree'] = x.rentree.astype(int)
+        df = df.rename(columns={'cometa':'cometa_orig'}).merge(x, how='left', on=['rentree', 'id_paysage'])
+        df.loc[df.cometa.isnull(), 'cometa'] = df.loc[df.cometa.isnull()].cometa_orig
+    except:
+        print("- COMETA not clean")
+    return df.drop_duplicates()
+
+def comins_clean(df):
+    try:
+        print(f"- COMINS clean")
+        comins_dict=json.load(open('patches/comins_fix.json', 'r'))
+        def get_first_value(comins_dict, key):
+            if key in comins_dict:
+                inner_dict = comins_dict[key]
+                # Retourne la première valeur du dictionnaire interne
+                return next(iter(inner_dict.values()))
+            return None
+
+        # Mise à jour de la colonne 'comins'
+        for i, row in df.iterrows():
+            key = row['compos']
+            if key in comins_dict:
+                df.at[i, 'comins'] = get_first_value(comins_dict, key)
+            
+        df.loc[df['etabli']==df['compos'], 'comins'] = df.loc[df['etabli']==df['compos'], 'cometa']
+
+
+        map_dict = json.load(open(f"patches/compos_comins.json", 'r'))
+        for rentree, compos_dict in map_dict.items():
+            for compos, comins in compos_dict.items():
+                mask = (df['rentree'] == int(rentree)) & (df['compos'] == compos)
+                df.loc[mask, 'comins'] = comins
+    except:
+        print("- COMINS not clean")
+
+    return df.rename(columns={'comins':'comui'}).drop_duplicates()
+
+
+
+#################################################################################################
+# INSPE / ESPE
+def etabli_meef(year):
+    meef = pd.read_pickle(f"{PATH}output/meef_frequency_source_year{year}.pkl",compression= 'gzip')
+    meef = meef[['rentree', 'source', 'etabli', 'etabli_diffusion', 'flag_meef']].drop_duplicates()
+    # create new_lib with correction etabli_diffusion normandie nouvelle caledonie
+    mapping_fix = json.load(open("patches/etabli_meef.json"))
+    def fix(row):
+        etabli = row['etabli']
+        flag = row['flag_meef']
+        if pd.notna(etabli) and etabli in mapping_fix and flag=='1':
+            return mapping_fix[etabli.upper()]
+        else:
+            return row['etabli_diffusion']
+        
+    meef['new_lib'] = meef.apply(fix, axis=1)
+    print(f"- size ETAB after meef process: {len(meef)}")
+
+    # keep only inspe espe and add id_paysage (id_paysage_formens)
+    meef = meef.loc[(meef.flag_meef=='1')&(meef.new_lib.str.lower().str.startswith(('inspe', 'espe'), na=False))]
+    meef_paysage = CORRECTIFS_dict['ETABLI_DIFFUSION_ID']
+    mapping = {
+    item['in']: item['out']
+    for item in meef_paysage
+    }
+    def add_paysage_id(row):
+        etabli = row['new_lib']
+        if pd.notna(etabli) and etabli.upper() in mapping:
+            return mapping[etabli.upper()]
+        else:
+            return np.nan
+
+    meef['id_paysage_formens'] = meef.apply(add_paysage_id, axis=1)
+    meef = meef[['rentree', 'source', 'etabli', 'etabli_diffusion', 'flag_meef', 'new_lib', 'id_paysage_formens']]
+
+    meef_null = meef.loc[meef.id_paysage_formens.isnull()].drop(columns='id_paysage_formens')
+    if len(meef_null)>0:
+        meef_null.to_csv(f"{PATH}work/meef_null.csv", sep=";", index=False)
+        print("- ATTENTION ! il manque des etabli_diffusion dans la googleshit ; vérifier dans work/meef_null")
+    return meef
+
+######################################################################################################################
 
 def delete(df):
     import pandas as pd
@@ -83,40 +298,7 @@ def vars_no_empty(df):
                 print(f"### {[str(x).isdigit() and x != 0 for x in liste_entiers]}")
 
 
-#####################################################
-## ETABLISSEMENT ####################################
-def comins_clean(df):
-    try:
-        print(f"- COMINS clean")
-        comins_dict=json.load(open('patches/comins.json', 'r'))
-        def get_first_value(comins_dict, key):
-            if key in comins_dict:
-                inner_dict = comins_dict[key]
-                # Retourne la première valeur du dictionnaire interne
-                return next(iter(inner_dict.values()))
-            return None
 
-        # Mise à jour de la colonne 'comins'
-        for i, row in df.iterrows():
-            key = row['ui']
-            if key in comins_dict:
-                df.at[i, 'comins'] = get_first_value(comins_dict, key)
-
-    except:
-        print("- COMINS not clean")
-
-    return df.rename(columns={'comins':'comui'})
-
-def cometa_clean(df):
-    try:
-        print(f"- COMETA clean")
-        x=pd.DataFrame(CORRECTIFS_dict['A_COMETA']) 
-        x['rentree'] = x.rentree.astype(int)
-        df = df.rename(columns={'cometa':'cometa_orig'}).merge(x, how='left', on=['rentree', 'id_paysage'])
-        df.loc[df.cometa.isnull(), 'cometa'] = df.loc[df.cometa.isnull()].cometa_orig
-    except:
-        print("- COMETA not clean")
-    return df
 
 
 ####################################################
