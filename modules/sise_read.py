ab088deccd7754ab599caf724dc9a91428871109
zoe <zmenesr@gmail.com>
2025-11-04 16:28:12 +0100

    .

--- modules/sise_read.py
+++ modules/sise_read.py
@@ -1,7 +1,7 @@
 def sise_read(path):
     import pandas as pd, numpy as np
     from modules.sise_content import zip_content, src_load, data_save, vars_init, rattach_init
-    from utils.functions_shared import get_sources, check_items_list
+    from utils.functions_shared import get_sources
     
 
     ### liste les noms des datasets présents dans le zip parquet, extraction de la dernière années des données dispo
@@ -12,7 +12,7 @@ def sise_read(path):
     # Chargement des tables et création d'une base complète df_all sauvé au format parquet par année
     ### Ajout des infos sur l'état des sources dans 
     # etat des variables par année source
-    df_items = pd.DataFrame()
+    # df_items = pd.DataFrame()
     uai_correctif = pd.DataFrame()
     meef = pd.DataFrame()
 
@@ -35,10 +35,10 @@ def sise_read(path):
 
         # sauvegarde dans output d'un sise complet par année au format parquet
         data_save(rentree, df_all, last_data_year)
-        res = check_items_list(df_all)
-        df_items = pd.concat([df_items, res])
+        # res = check_items_list(df_all)
+        # df_items = pd.concat([df_items, res])
         
-        uai_correctif = pd.concat([uai_correctif, df_all[['rentree', 'source', 'etabli', 'compos', 'rattach', 'effectif']]])
+        uai_correctif = pd.concat([uai_correctif, df_all[['rentree', 'source', 'etabli', 'compos', 'rattach', 'cometa', 'comins', 'effectif']]])
         
         if 'etabli_diffusion' in df_all.columns or 'flag_meef' in df_all.columns:
             meef = pd.concat([meef, df_all.loc[~df_all.etabli_diffusion.isnull(), ['rentree', 'source', 'etabli', 'etabli_diffusion', 'flag_meef', 'typ_dipl', 'diplom', 'effectif']]])
@@ -51,10 +51,10 @@ def sise_read(path):
         .reset_index()
         .to_pickle(f"{path}output/meef_frequency_source_year{last_data_year}.pkl",compression={'method': 'gzip', 'compresslevel': 1, 'mtime': 1}))
     
-    (uai_correctif.groupby(['rentree', 'source', 'etabli', 'compos', 'rattach'], dropna=False)
+    (uai_correctif.groupby(['rentree', 'source', 'etabli', 'compos', 'rattach', 'cometa', 'comins'], dropna=False)
                .agg(effectif_tot=('effectif', 'sum'), count_rows=('effectif', 'size'))
                .reset_index()
                .to_pickle(f"{path}output/uai_frequency_source_year{last_data_year}.pkl",compression={'method': 'gzip', 'compresslevel': 1, 'mtime': 1}))
     
     # df_items.mask(df_items=='', inplace=True)
-    df_items.to_pickle(f"{path}output/items_origin_by_vars{last_data_year}.pkl",compression={'method': 'gzip', 'compresslevel': 1, 'mtime': 1})
+    # df_items.to_pickle(f"{path}output/items_origin_by_vars{last_data_year}.pkl",compression={'method': 'gzip', 'compresslevel': 1, 'mtime': 1})
